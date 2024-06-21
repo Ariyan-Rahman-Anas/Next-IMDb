57f810f80ad0ad56b052d02124461ca6aee8ac2b
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-06-21 22:04:51 +0600

    created new section called HomeNavbar for fetching data

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import HomeNavbar from "../components/HomeNavbar";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <div className="flex items-center justify-start gap-4 mt-6 ">
+        <HomeNavbar param={"fetchTrending"} title="Trending" />
+      <HomeNavbar param={"fetchTopRated"} title="Top Rated" />
+      </div>
     </div>
   )
-}''
+}
---
+++ src/components/HomeNavbar.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation"
+
+export default function HomeNavbar({param, title}){
+
+    const searchParams= useSearchParams()
+    const genre = searchParams.get("genre")
+
+    return (
+      <Link
+        param="fetchTrending"
+        href={`/?genre=${param}`}
+        className={`${
+          genre === param ? "underline underline-offset-4 text-gray-100 " : ""
+        } hover:text-gray-100 font-semibold duration-500 `}
+      >
+        {title}
+      </Link>
+    );
+}
